11a25764375f104cbf0e4bc3166aca740ce6a171
sabrina <sabrinaabrodi20@gmail.com>
2022-10-23 09:38:44 +0200

    feat: adding dom elements

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,11 @@
 <script>
-	export let name;
+	let count = 1;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>{count}</p>
+	<button on:click={() => count++}>+</button>
+	<button on:click={() => count--}>-</button>
 </main>
 
 <style>
@@ -15,16 +16,4 @@
 		margin: 0 auto;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
